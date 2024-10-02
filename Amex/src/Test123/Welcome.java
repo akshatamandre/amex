53adce11f9a3c8f3e8f8e458560ffa81ce0c9ba0
akshatamandre <makshata9@gmail.com>
2024-10-02 16:52:28 -0700

    trying for merge conflict

--- Amex/src/Test123/Welcome.java
+++ Amex/src/Test123/Welcome.java
@@ -5,7 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("Welcome to Amex");
-    System.out.println("Hi!!Akshata");
+    System.out.println("Hi!!Akshata Mandre");
 	}
 
 }
